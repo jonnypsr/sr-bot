d6712df9031c080655ff11208add586f857337c4
jonnypsr <jonnyguitar10@hotmail.com>
2017-08-04 10:25:07 +0100

    Added my project

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 var Discordie = require("discordie");
 var S = require('string');
 var Events = Discordie.Events;
-var http = require('http');
-
- http.createServer(function (request, response) {
-
 var client = new Discordie();
 
 client.connect({ token: "MzQyOTE2ODA4MTk1NTcxNzEz.DGWl3Q.pOKLqyCMRcru7OFWBVFwkvoOwpE" });
 
 client.Dispatcher.on(Events.GATEWAY_READY, e => {
 console.log("Connected as: " + client.User.username);
-});
+}).listen(process.env.PORT);
 
 client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 if (S(e.message.content).startsWith("--setrank"))
@@ -23,6 +19,4 @@ else if (e.message.content == "ping")
 {
 	e.message.channel.sendMessage("pong");
 }
-});
-                    response.end();
- }).listen(process.env.PORT);
+}).listen(process.env.PORT);
