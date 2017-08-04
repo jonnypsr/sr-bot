d4fc00b4ac0b430f05ddf614b384dbfa99c1d13a
jonnypsr <jonnyguitar10@hotmail.com>
2017-08-04 10:44:07 +0100

    added

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var Discordie = require("discordie");
+var Discord = require("discord.js");
 var S = require('string');
-var Events = Discordie.Events;
 var http = require('http');
 
  http.createServer(function (request, response) {
 
-var client = new Discordie();
+var client = new Discord.Client();
 
-client.connect({ token: "MzQyOTE2ODA4MTk1NTcxNzEz.DGWl3Q.pOKLqyCMRcru7OFWBVFwkvoOwpE" });
+client.login("MzQyOTE2ODA4MTk1NTcxNzEz.DGWl3Q.pOKLqyCMRcru7OFWBVFwkvoOwpE");
 
 client.Dispatcher.on(Events.GATEWAY_READY, e => {
 console.log("Connected as: " + client.User.username);
