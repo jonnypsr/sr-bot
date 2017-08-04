4da27095e5db2350025825a248ef1aa8e905c0f7
jonnypsr <jonnyguitar10@hotmail.com>
2017-08-04 11:08:59 +0100

    added

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ var S = require('string');
 var Events = Discordie.Events;
 var client = new Discordie();
 
-client.connect({ token: "MzQyOTE2ODA4MTk1NTcxNzEz.DGWl3Q.pOKLqyCMRcru7OFWBVFwkvoOwpE",
-autoReconnect: true});
+client.connect({ token: "MzQyOTE2ODA4MTk1NTcxNzEz.DGWl3Q.pOKLqyCMRcru7OFWBVFwkvoOwpE"});
 
 client.Dispatcher.on(Events.GATEWAY_READY, e => {
 console.log("Connected as: " + client.User.username);
